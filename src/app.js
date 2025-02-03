064374b2c9b3aa76bd1262ecd7f04514c449a542
JobertDev27 <jobertdev27@gmail.com>
2025-02-03 18:42:03 +0800

    Multiple addition
    - added dom for task elements
    - fixed storage.js
    - removed sidebar
    - removed filter, too much work for dom manipulation

--- src/app.js
+++ src/app.js
@@ -1,1 +1,49 @@
+import {reloadStorage, storeStorage} from './storage.js';
+import createDom from './dom.js';
 import "./style.css";
+
+// Handles the form hiding and showing
+const formHandler = () => {
+    console.log(localStorage.getItem("myItems"))
+
+    const formContainer = document.getElementById("add-menu");
+    const addTodo = document.getElementById("add-task-button");
+    const form = document.querySelector("form");
+
+    let tasks = [];
+    console.log(reloadStorage())
+
+    // HIDE THE FORM BY CLICKING OUTSIDE
+    formContainer.addEventListener("click", (e) => {
+        if (e.target === formContainer){
+            hideForm();
+        }
+    });
+
+    // SHOWS FORM BY REMOVINNG HIDDEN CLASS
+    addTodo.addEventListener("click", () => formContainer.classList.remove("hidden"));
+
+    // HIDES THE FORM, SEPERATED IN A FUNCTION FOR REUSE
+    function hideForm () {
+        formContainer.classList.add("hidden");
+    }
+
+    // HANDLES FORM SUBMISSION
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const taskData = new FormData(form); 
+        const data = Object.fromEntries(taskData);
+
+        tasks.push(data);
+        createDom(tasks.at(-1));
+        console.log(tasks);
+        storeStorage(tasks);
+        
+        hideForm();
+        form.reset();
+    })
+
+    
+}
+
+formHandler();
